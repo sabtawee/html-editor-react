3f29713ccf9ee696d45dcee18966df257b5c9cba
Sabtawee <sabtawee.bank@gmail.com>
2023-03-05 12:17:45 +0700

    main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyEditor from "./components/MyEditor/MyEditor";
+
+function App() {
+  return (
+    <div>
+      <MyEditor />
+    </div>
+  );
+}
+
+export default App;
